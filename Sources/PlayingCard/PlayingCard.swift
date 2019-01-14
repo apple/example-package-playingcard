177a763fa2580f969645000f7c41af91f3bb5e58
Mattt <mattt@me.com>
2019-01-14 07:28:12 -0800

    Provide public access to rank and suit properties

--- Sources/PlayingCard/PlayingCard.swift
+++ Sources/PlayingCard/PlayingCard.swift
@@ -9,8 +9,8 @@
 */
 
 public struct PlayingCard {
-    let rank: Rank
-    let suit: Suit
+    public let rank: Rank
+    public let suit: Suit
 
     public init(rank: Rank, suit: Suit) {
         self.rank = rank
